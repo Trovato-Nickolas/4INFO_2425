667cb6ff307d12cc0733231b7ecc0e1702a40a0e
Trovato-Nickolas <154589467+Trovato-Nickolas@users.noreply.github.com>
2025-03-06 19:38:14 +0100

    Delete esercizio_js_60 directory

--- esercizio_js_60/soluzione.js
+++
@@ -1,9 +0,0 @@
-export function is_anagram(arr) {
-    const groups = arr.reduce((map, word) => {
-        const key = word.split('').sort().join('');
-        map.set(key, (map.get(key) || 0) + 1);
-        return map;
-    }, new Map());
-
-    return arr.filter(word => groups.get(word.split('').sort().join('')) === 1);
-}
