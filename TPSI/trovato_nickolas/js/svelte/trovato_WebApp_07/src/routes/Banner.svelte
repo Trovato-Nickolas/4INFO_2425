0492d9e06ff74a35e4c53acce3583664466c7ccd
Trovato-Nickolas <154589467+Trovato-Nickolas@users.noreply.github.com>
2025-04-04 13:45:32 +0200

    add es svelte dal 7 al 9

---
+++ TPSI/trovato_nickolas/js/svelte/trovato_WebApp_07/src/routes/Banner.svelte
@@ -0,0 +1,14 @@
+<script>
+	let name = "Nickolas Trovato"
+</script>
+
+<h1>My name is {name}!</h1>
+
+<style>
+	h1{
+		padding: 5%;
+		border: solid 10px green;
+		border-radius: 10px;
+	}
+</style>
+
