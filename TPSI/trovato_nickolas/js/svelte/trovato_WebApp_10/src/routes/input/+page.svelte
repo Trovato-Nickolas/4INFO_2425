7f63313e583aece35f923bb1daf13aa404e01ede
Trovato-Nickolas <154589467+Trovato-Nickolas@users.noreply.github.com>
2025-04-11 13:39:16 +0200

    add es 10 11 12

---
+++ TPSI/trovato_nickolas/js/svelte/trovato_WebApp_10/src/routes/input/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    let nome , cognome , eta;
+</script>
+
+<h1>
+    Input Page
+</h1>
+
+<ul>
+    <li>Nome <input type="text" bind:value={nome}></li>
+    <li>Cognome <input type="text" bind:value={cognome} ></li>
+    <li>Eta <input type="text" bind:value={eta}></li>
+</ul> 
+<br>
+<p>Io Sono {nome} {cognome} e ho {eta} anni</p>
---
+++ TPSI/trovato_nickolas/js/svelte/trovato_WebApp_11/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<div>
+    <h1>WELCOME to my WebApp</h1>
+</div>
+
+
+<style>
+    div{
+        height: 75vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: solid blue 2px;
+    }
+</style>
