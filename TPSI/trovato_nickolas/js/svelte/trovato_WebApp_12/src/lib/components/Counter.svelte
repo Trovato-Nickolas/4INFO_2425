f027ce196d97430e050d21b148b0faa1ec16c834
Trovato-Nickolas <154589467+Trovato-Nickolas@users.noreply.github.com>
2025-04-11 13:39:58 +0200

    add es 12

---
+++ TPSI/trovato_nickolas/js/svelte/trovato_WebApp_12/src/lib/components/Counter.svelte
@@ -0,0 +1,65 @@
+<svelte:head>
+  <link href="https://fonts.googleleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" rel=stylesheet/>
+  <link href="https://fonts.googleleapis.com/css2?family=Bungee+Spice&display=swap rel=stylesheet"/>
+</svelte:head>
+
+<script>
+    export let counter = 0;
+  
+    function incrementa() {
+      counter ++ ;
+    }
+
+    function decrementa() {
+      counter --;
+    }
+  </script>
+
+<h1>Pagina Coutner</h1>
+
+<div class="container">
+    <div class="pulsante">
+        <button on:click={decrementa}>
+            <span class="materiale-symbols-outline icon">-</span>
+        </button>
+    </div>
+    <div class="number">
+        {counter}
+    </div>
+    <div class="pulsante">
+        <button on:click={incrementa}>
+            <span class="materiale-symbols-outline icon">+</span>
+        </button>
+    </div>
+</div>
+
+<style>
+    .container{
+        border: 4px  solid #d43c2f;
+        display: flex;
+        justify-content: space-between;
+        border-radius:  30px;
+        width: 300px;
+        padding: 10px;
+    }
+    .pulsante{
+        display: flex;
+        align-items: center;
+    }
+    button{
+        border: none;
+        background: none;
+    }
+    .icon {
+        font-size: 2.5rem;
+        color: #f09c00;
+        font-weight: bolder;
+    }
+    .icon:hover{
+        text-shadow: 1px 1px 10px #d43c2f;
+    }
+    .number{
+        font-size: 4rem;
+        font-family: 'Bungee Splice' ,cursive;
+    }
+</style>
---
+++ TPSI/trovato_nickolas/js/svelte/trovato_WebApp_12/src/routes/counter/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Counter from "$lib/components/Counter.svelte";
+    import DoubleCounter from "$lib/components/DoubleCounter.svelte";
+    let contatore = 0
+    let numeri = [1,2,3,4,5,6,7]
+</script>
+
+<h1>Pagina COunter !!</h1>
+
+<Counter/>
+
+<DoubleCounter bind:counter={contatore}/>
+
+{#if contatore%5}
+   <h2 class="rosso">Il Valore del contatore vale : {contatore} </h2>
+{:else}
+   <h2 class="verde">Il Valore del contatore vale : {contatore} </h2>
+{/if}
+
+<ul>
+     {#each numeri as numero,indice}
+        <li>numeri[{indice}]:{numero}</li>
+     {/each}
+</ul>
+
+<style>
+    .rosso{color: red;}
+    .verde{color: green;}
+</style>
