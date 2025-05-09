dc900b34f07cc64caf70f9e5775cc093dc9d635a
Trovato-Nickolas <154589467+Trovato-Nickolas@users.noreply.github.com>
2025-05-09 13:16:04 +0200

    add es dal 15 a 16

---
+++ TPSI/trovato_nickolas/js/svelte/trovato_WebApp_15/src/routes/chi_sono/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import WhoAmI from "$lib/components/who_am_i.svelte";
+</script>
+
+<WhoAmI nome ="Nickolas" cognome = "s" eta="20" src = "https://i0.wp.com/borgovergini.it/wp-content/uploads/2021/07/avatar-uomo.jpg?ssl=1" width=100 height=100 />
+<WhoAmI nome ="sad" cognome = "sas" eta="20"/>
---
+++ TPSI/trovato_nickolas/js/svelte/trovato_WebApp_15/src/routes/input/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    let nome , cognome , eta;
+    let confermato = false;
+</script>
+
+<h1>
+    Input Page
+</h1>
+
+<ul>
+    <li>Nome <input type="text" bind:value={nome}></li>
+    <li>Cognome <input type="text" bind:value={cognome} ></li>
+    <li>Eta <input type="text" bind:value={eta}></li>
+</ul> 
+<br>
+<p>Confermato <input type="checkbox" bind:checked={confermato}></p>
+<br>
+
+<p>Io Sono {nome} {cognome} e ho {eta} anni</p>
+
+<button disabled = {!confermato}>Invia</button>
+
